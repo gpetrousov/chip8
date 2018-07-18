645becb1cfa00fc2a8e47d1fb6a15648ed4ae4d5
Ioannis Petrousov <petrousov@gmail.com>
2018-07-18 16:40:26 +0200

    Keyboard input and opcodes for input

--- main.go
+++ main.go
@@ -53,6 +53,27 @@ func main() {
 		0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 	}
 
+	keys := map[uint8]uint8{
+		0x0: 0,
+		0x1: 0,
+		0x2: 0,
+		0x3: 0,
+
+		0x4: 0,
+		0x5: 0,
+		0x6: 0,
+		0x7: 0,
+		0x8: 0,
+		0x9: 0,
+
+		0xA: 0,
+		0xB: 0,
+		0xC: 0,
+		0xD: 0,
+		0xE: 0,
+		0xF: 0,
+	}
+
 	// Initialize PC
 	pc = 0x200
 
@@ -61,13 +82,29 @@ func main() {
 		memory[i] = v
 	}
 
+	// Darwin instructions
+	exec.Command("stty", "-f", "/dev/tty", "cbreak", "min", "1").Run() // Disable input buffering
+	exec.Command("stty", "-f", "/dev/tty", "-echo").Run()              // Do not display entered characters on the screen
+	// Linux instructions
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run() // Disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()              // Do not display entered characters on the screen
+	// Routine to read from keyboard
+	var b []byte = make([]byte, 1)
+	go func() {
+		for {
+			os.Stdin.Read(b)
+			fmt.Println("I got the byte", b, "("+string(b)+")")
+			bounceKey(b, keys)
+		}
+	}()
+
 	// MAin loop
 	for {
 		// Fetch
 		opcode := fetchOpcode(memory, pc)
 
 		// Decode
-		decodeOpcode(opcode, stack, pc, sp, vReg, iReg, delayTimer, soundTimer, memory, screen)
+		decodeOpcode(opcode, stack, pc, sp, vReg, iReg, delayTimer, soundTimer, memory, screen, keys)
 
 		// Execute
 
@@ -75,23 +112,7 @@ func main() {
 
 		// Update timers
 
-		// Darwin instructions
-		exec.Command("stty", "-f", "/dev/tty", "cbreak", "min", "1").Run() // Disable input buffering
-		exec.Command("stty", "-f", "/dev/tty", "-echo").Run()              // Do not display entered characters on the screen
-
-		// Linux instructions
-		exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run() // Disable input buffering
-		exec.Command("stty", "-F", "/dev/tty", "-echo").Run()              // Do not display entered characters on the screen
-
-		// Routine to read from keyboard
-		var b []byte = make([]byte, 1)
-		go func() {
-			for {
-				os.Stdin.Read(b)
-				fmt.Println("I got the byte", b, "("+string(b)+")")
-			}
-		}()
-
+		debounceKeys(keys)
 	}
 
 }
@@ -102,7 +123,8 @@ func fetchOpcode(memory [4096]uint8, pc uint16) uint16 {
 	return opcode
 }
 
-func decodeOpcode(opcode uint16, stack [16]uint16, pc uint16, sp uint16, vReg [16]uint8, iReg uint16, delayTimer uint8, soundTimer uint8, memory [4096]uint8, screen [64 * 32]uint8) {
+// Decode and execute opcode, increase PC
+func decodeOpcode(opcode uint16, stack [16]uint16, pc uint16, sp uint16, vReg [16]uint8, iReg uint16, delayTimer uint8, soundTimer uint8, memory [4096]uint8, screen [64 * 32]uint8, keys map[uint8]uint8) {
 
 	// Zero opcodes
 	switch opcode {
@@ -256,8 +278,18 @@ func decodeOpcode(opcode uint16, stack [16]uint16, pc uint16, sp uint16, vReg [1
 		switch opcode & 0x00F0 {
 		case 0x0090:
 			fmt.Println("Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)")
+			if keys[vReg[(opcode&0x0F00)>>8]] == 1 {
+				pc += 4
+			} else {
+				pc += 2
+			}
 		case 0x00A0:
 			fmt.Println("Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)")
+			if keys[vReg[(opcode&0x0F00)>>8]] != 1 {
+				pc += 4
+			} else {
+				pc += 2
+			}
 		}
 
 	// F opcodes
@@ -341,4 +373,55 @@ func updateTimers(soundTimer uint8, delayTimer uint8) {
 		}
 	}
 	soundTimer -= 1
+}
+
+// Bounce key by updating it's state (key pressed)
+func bounceKey(k []byte, keys map[uint8]uint8) {
+	switch string(k) {
+	case "1":
+		keys[0x1] = 1
+	case "2":
+		keys[0x2] = 1
+	case "3":
+		keys[0x3] = 1
+	case "4":
+		keys[0xC] = 1
+
+	case "q":
+		keys[0x4] = 1
+	case "w":
+		keys[0x5] = 1
+	case "e":
+		keys[0x6] = 1
+	case "r":
+		keys[0xD] = 1
+
+	case "a":
+		keys[0x7] = 1
+	case "s":
+		keys[0x8] = 1
+	case "d":
+		keys[0x9] = 1
+	case "f":
+		keys[0xE] = 1
+
+	case "z":
+		keys[0xA] = 1
+	case "x":
+		keys[0x0] = 1
+	case "c":
+		keys[0xB] = 1
+	case "v":
+		keys[0xF] = 1
+
+	default:
+		fmt.Println("Unknown key pressed")
+	}
+}
+
+// Set all keys to zero/debounce
+func debounceKeys(k map[uint8]uint8) {
+	for i, _ := range k {
+		k[i] = 0
+	}
 }
